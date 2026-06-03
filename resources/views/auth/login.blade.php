c31b6b1b47f4eb3e199355f62007c791003b1fa6
elsafebrianita <elsafebrianita1702@gmail.com>
2026-06-03 14:15:12 +0700

    test cok

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -108,8 +108,7 @@
 <body>
 
     <div class="login-card">
-        <h2>Selamat Datang COK</h2>
-        <h3>woilah bisa</h3>
+        <h2>Selamat Datang</h2>
         <p>Silakan login untuk masuk ke sistem</p>
 
         @if(session('success'))
